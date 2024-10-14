661185d4721ca6d04669d0879be0abace21ead09
Karl Swedberg <kswedberg@gmail.com>
2024-10-14 15:21:10 -0400

    Merge assets into both the tabs and the options page for Orion

--- scripts/chrome2orion.mjs
+++ scripts/chrome2orion.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
@@ -5,35 +6,54 @@ const chromeFiles = {
   js: path.join(process.cwd(), 'dist/chrome/tab/tab.js'),
   css: path.join(process.cwd(), 'dist/chrome/tab/tab.css'),
 };
+const pages = [
+  {
+    title: 'New Tab Bookmarks',
+    js: path.join(process.cwd(), 'dist/chrome/tab/tab.js'),
+    css: path.join(process.cwd(), 'dist/chrome/tab/tab.css'),
+    dest: path.join(process.cwd(), 'orion/tab/tab.html'),
+  },
+  {
+    title: 'New Tab Settings',
+    js: path.join(process.cwd(), 'dist/chrome/tab/tab.js'),
+    css: path.join(process.cwd(), 'dist/chrome/tab/tab.css'),
+    dest: path.join(process.cwd(), 'orion/tab/tab.html'),
+  },
+];
+const opts = {encoding: 'utf8'};
 
 const run = async() => {
-  const opts = {encoding: 'utf8'};
 
-  const tabFile = path.join(process.cwd(), 'orion/tab/tab.html');
-  const css = await fs.readFile(chromeFiles.css, opts);
-  const js = await fs.readFile(chromeFiles.js, opts);
-  const jsCleaned = js.replace(/chrome:\/\//g, 'orion://');
-  const html = [
-    '<!DOCTYPE html>',
-    '<html lang="en-US">',
-    '<head>',
-    '  <meta charset="utf-8"/>',
-    '  <title>New Tab Bookmarks</title>',
-    '  <style>',
-    `  ${css}`,
-    '  </style>',
-    '</head>',
-    '<body class="orion">',
-    '  <div id="app"></div>',
-    '  <script id="t-script">',
-    `  ${jsCleaned}`,
-    '  </script>',
-    '</body>',
-    '</html>',
-  ].join('\n');
+  for (let page of pages) {
+    const css = await fs.readFile(page.css, opts);
+    const js = await fs.readFile(page.js, opts);
+    let jsCleaned = js.replace(/chrome:\/\//g, 'orion://');
 
-  await fs.writeFile(tabFile, html);
-  console.log('Updated Orion build');
+    if (page.css.includes('tab')) {
+      jsCleaned = jsCleaned.replace(/À[^}]+/, '');
+    }
+    const html = [
+      '<!DOCTYPE html>',
+      '<html lang="en-US">',
+      '<head>',
+      '  <meta charset="utf-8"/>',
+      `  <title>${page.title}</title>`,
+      '  <style>',
+      `  ${css}`,
+      '  </style>',
+      '</head>',
+      '<body class="orion">',
+      '  <div id="app"></div>',
+      '  <script id="t-script">',
+      `  ${jsCleaned}`,
+      '  </script>',
+      '</body>',
+      '</html>',
+    ].join('\n');
+
+    await fs.writeFile(page.dest, html);
+    console.log(`Updated ${page.title} for Orion`);
+  }
 };
 
 run();
